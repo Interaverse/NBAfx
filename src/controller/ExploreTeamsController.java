6e5ea24e47a6c4f151e394cea3a6ec7478d65168
interaverse <equinoxteam001@gmail.com>
2023-05-09 01:50:44 +1000

    Initial commit

---
+++ src/controller/ExploreTeamsController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import au.edu.uts.ap.javafx.ViewLoader;
+import au.edu.uts.ap.javafx.Controller;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.stage.*;
+import javafx.scene.image.Image;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import model.Association;
+import model.Teams;
+
+public class ExploreTeamsController extends Controller<Teams> {
+    @FXML
+    private Button teamMenuButton;
+
+    @FXML
+    private Button viewPlayersButton;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private GridPane buttonGrid;
+
+    //add button function from ExploreTeamsView.fxml here
+    public void openTeamMenu() {
+
+    }
+    public void viewPlayers() {
+
+    }
+    public void close() {
+        //close the window
+        stage.close();
+    }
+
+    
+}
+

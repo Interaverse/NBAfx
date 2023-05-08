8c70d7e086c873f20a6f448fe6bf351eea9adbd7
Shayan Razavi <equinoxteam001@gmail.com>
2023-05-09 04:33:10 +1000

    changes from pc

--- src/controller/SeasonController.java
+++ src/controller/SeasonController.java
@@ -1,7 +1,60 @@
 package controller;
 
+import au.edu.uts.ap.javafx.ViewLoader;
+import au.edu.uts.ap.javafx.Controller;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.stage.*;
+import javafx.scene.image.Image;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import model.Season;
 
-public class SeasonController  {
+public class SeasonController  extends Controller<Season>{
+    //add private fields from SeasonView.fxml here named on fx:id
+    @FXML
+    private Button roundButton;
+
+    @FXML
+    private Button currentButton;
+
+    @FXML
+    private Button gameButton;
+
+    @FXML
+    private Button resultButton;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    public void addRound() {
+
+    }
+
+    @FXML
+    public void displayRound() {
+
+    }
+
+    @FXML
+    public void playGame() {
+
+    }
+
+    @FXML
+    public void displayResult() {
+
+    }
+
+    @FXML
+    public void close() {
+
+    }
+
 
 }
 
